6d7a1ba4091be1f9f9099c91885b6ce85be18dc7
Ivan <i.ryapolov.79@.gmail.com>
2019-02-25 13:32:31 +0200

    Цикл

--- 1 data type/tempCodeRunnerFile.js
+++ 1 data type/tempCodeRunnerFile.js
@@ -1,37 +1,9 @@
-let num = 50;
-
-if (num < 49) {
-  console.log('Неверно!');
-} else if (num > 100) {
-  console.log('Много!');
-} else {
-  console.log('Верно!');
+for (let i = 0; i < 15; i++) {
+  if (i == 7) {
+    continue
+  }
+  if (i == 14) {
+    break
+  }
+  console.log(i);
 };
-
-
-// тернарный оператор
-(num == 50) ? console.log('Верно!') : console.log('Неверно!');
-
-
-// Конструкция swich
-
-switch (num) {
-  case num < 49:
-    console.log('Неверно!');
-    break;
-  case num > 100:
-    console.log('Много!');
-    break;
-  case num > 800:
-    console.log('Все еще много!');
-    break;
-  case 50:                                  // неправильная запись num == 50
-    console.log('Верно!');
-    break;
-  default:
-    console.log('Что-то пошло не так!');
-    break;
-}
-
-
-
--- HW1/main.js
+++ HW1/main.js
@@ -1,7 +1,8 @@
 // 'use strict'; 
 
-let money = +prompt("Ваш бюджет на месяц?", ""),
-    time = prompt("Введите дату в формате YYYY-MM-DD", "");
+let money = +prompt("Ваш бюджет на месяц?", ''),
+    time = prompt("Введите дату в формате YYYY-MM-DD", '');
+
 let appData = {
     budget: money,
     timeData: time,
@@ -10,10 +11,37 @@ let appData = {
     income: [],
     savings: false
 };
-let point1 = +prompt("Введите обязательную статью расходов в этом месяце", ""),
-    point2 = +prompt("Во сколько обойдется?", ""),
-    point3 = +prompt("Введите обязательную статью расходов в этом месяце?", ""),
-    point4 = +prompt("Во сколько обойдется?", "");
+
+for (let i = 0; i < 2; i++) {
+  let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+      b = prompt("Во сколько обойдется?", '');
+  
+  if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null
+      && a != '' &&  b != '' && a.length < 50) {
+        console.log("done");
+        appData.expenses[a] = b;
+      } else {
+          i = i - 1;
+      }
+}
+
+// let i = 0;
+// while (i < 2){
+//   let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+//       b = prompt("Во сколько обойдется?", '');
+//   i++;
+// }
+
+
+// do {
+//   let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+//       b = prompt("Во сколько обойдется?", '');
+//   i++;
+// }
+// while (i < 2);
+
+
+appData.moneyPerDay = appData.budget / 30;
 
 alert("Ежедневный бюджет: " + appData.moneyPerDay);
 
