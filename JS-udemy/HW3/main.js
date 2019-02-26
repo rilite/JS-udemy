90884c9aeb75e218bea71f3917a2c456db523729
Ivan <i.ryapolov.79@.gmail.com>
2019-02-26 09:36:09 +0200

    Функции

---
+++ JS-udemy/HW3/main.js
@@ -0,0 +1,81 @@
+let money, time;
+
+function start() {
+  money = +prompt("Ваш бюджет на месяц?", '');
+  time = prompt("Введите дату в формате YYYY-MM-DD", '');
+
+  while(isNaN(money) || money == "" || money == null) {
+    money = +prompt("Ваш бюджет на месяц?", '');
+  }
+}
+start();
+
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: true
+};
+
+function chooseExpenses() {
+  for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+        b = prompt("Во сколько обойдется?", '');
+    
+    if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null
+        && a != '' &&  b != '' && a.length < 50) {
+          console.log("done");
+          appData.expenses[a] = b;
+        } else {
+            i = i - 1;
+        }
+  }
+}
+chooseExpenses();
+
+function detectDayBudget() {
+  appData.moneyPerDay = (appData.budget / 30).toFixed();
+  alert("Ежедневный бюджет: " + appData.moneyPerDay);
+}
+detectDayBudget();
+
+function detectLeve() {
+  if (appData.moneyPerDay < 100) {
+    console.log("Минимальный уроень достатка");
+  } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("Средний уроень достатка");
+  } else if (appData.moneyPerDay > 2000) {
+    console.log("Высокий уроень достатка");
+  } else {
+    console.log("Произошла ошибка");
+  }
+}
+detectLeve();
+
+function checkSavings() {
+  if (appData.savings == true) {
+    let save = +prompt("Какова сумма накоплений?", ""),
+        persent = +prompt("Под какой процент?", "");
+
+    appData.monthIncome = save / 100 / 12 * persent;
+    alert("Доход в месяц с вашего депозита " + appData.monthIncome);
+  }
+}
+checkSavings();
+
+function chooseOptExpenses() {
+  for (let i=0; i < 3; i++) {
+    let opt = prompt("Статья необязательных расходов?", "");
+    appData.optionalExpenses[i] = opt;
+  }
+}
+chooseOptExpenses();
+
+
+
+
+
+
+
